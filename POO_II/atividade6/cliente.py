ca046576de33c74052a07baafdc0d3b04ec64110
Saul Rocha <55061070+saul-rocha@users.noreply.github.com>
2021-10-24 15:14:15 -0300

    atividade 6 :white_check_mark:

---
+++ POO_II/atividade6/cliente.py
@@ -0,0 +1,22 @@
+import socket
+#ip = input("digite o ip de conexão: ")
+
+ip = "localhost"
+port = 8000
+addr = ((ip,port)) ##tupla  de endereço
+
+cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET parametro para informar a familia do protocolo, SOCK_STREAM indica que é TCP/IP
+cliente_socket.connect(addr) #conexão
+
+while(True):
+    mensagem = input("Digite uma mensagem para enviar ao servidor: ")
+    cliente_socket.send(mensagem.encode()) #send message
+    #print("Mensagem enviada")
+    recebe = cliente_socket.recv(1024).decode()
+    if(recebe != '/quit'):
+        print("mensagem recebida:" + recebe)
+    else:
+        break
+
+
+cliente_socket.close() #fecha conexão
---
+++ POO_II/atividade6/servidor.py
@@ -0,0 +1,27 @@
+import socket
+
+host = 'localhost'
+port = 8000
+addr = (host, port)
+
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria o socket
+serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reinicializa o socket
+serv_socket.bind(addr) #define a porta e quais ips podem se conectar ao servidor
+serv_socket.listen(10) #define o limite de conexões
+print("aguardando conexão...")
+con, cliente = serv_socket.accept() #server aguardando conexão
+print("conectado")
+print("aguardando mensagem")
+enviar = ''
+
+while(True):
+    recebe = con.recv(1024).decode() #define os pacotes recebidos são de ate 1024 bytes
+    print("mensagem recebida: " + recebe)
+    if(recebe != '/quit' and enviar != '/quit'):
+        enviar = input("Digite uma mensagem para enviar ao cliente: ")
+        con.send(enviar.encode())
+    else:
+        break
+
+con.send('/quit'.encode())
+serv_socket.close()
